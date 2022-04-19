e2653293f974fea9c5308312a206a948c4b61bae
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-19 13:52:38 -0300

    fix: organize folders following atomic design

--- lib/fruit_design_system.dart
+++ lib/fruit_design_system.dart
@@ -2,4 +2,5 @@ library fruit_design_system;
 
 export 'src/constants/unit.dart';
 export 'src/atomic/molecules/all.dart';
+export 'src/atomic/atoms/all.dart';
 export 'src/theme/theme.dart';
--- lib/src/atomic/molecules/all.dart
+++ lib/src/atomic/molecules/all.dart
@@ -1,3 +1,2 @@
-export 'box_spacer/box_spacer.dart';
 export 'primary_button/primary_button.dart';
 export 'secondary_button/secondary_button.dart';
