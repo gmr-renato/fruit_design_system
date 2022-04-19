4f7675aaaa2c9fafe7d7a3f20a738c5b7cfc303a
Renato M. Gonçalves <gmr.renato@gmail.com>
2022-04-19 07:47:59 -0300

    feat(theme): bring initial theme files
    This creates files for ColorScheme, TextTheme, ThemeData and FruitTheme

---
+++ test/src/theme/color_scheme.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: avoid_redundant_argument_values
+
+part of 'theme.dart';
+
+final fruitColorScheme = ColorScheme.light(
+  primary: const Color(0xFFE32E34),
+  onPrimary: Colors.white,
+  primaryContainer: const Color.fromARGB(255, 180, 49, 53),
+  onPrimaryContainer: Colors.white,
+  secondary: Colors.grey.shade900,
+  onSecondary: Colors.white,
+  background: Colors.grey.shade100,
+  onBackground: Colors.grey.shade900,
+  surface: Colors.grey.shade50,
+  onSurface: Colors.grey.shade900,
+  onSurfaceVariant: Colors.grey.shade800,
+  error: Colors.red,
+);
---
+++ test/src/theme/text_theme.dart
@@ -0,0 +1,11 @@
+part of 'theme.dart';
+
+TextTheme fruitPrimaryTextTheme = Typography.material2018().black.apply(
+      bodyColor: fruitColorScheme.onSurface,
+      displayColor: fruitColorScheme.onSurfaceVariant,
+    );
+
+TextTheme fruitSecondaryTextTheme = Typography.material2018().black.apply(
+      bodyColor: fruitColorScheme.onPrimary,
+      displayColor: fruitColorScheme.onPrimaryContainer,
+    );
